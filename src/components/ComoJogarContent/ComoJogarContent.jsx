4c77ff5ebd725f58b632f7e284b359750a9978de
wcmF0 <wagnerfilho696@gmail.com>
2025-04-30 18:41:04 -0300

    fix: update text content and improve grammar in ComoJogarContent fix: change footer background color for better visibility

--- src/components/ComoJogarContent/ComoJogarContent.jsx
+++ src/components/ComoJogarContent/ComoJogarContent.jsx
@@ -7,31 +7,33 @@ const ComoJogarContent = () => {
       <div className="comojogar-regras">
         <ul>
           <li className="comojogar-padrao">
-            <h2>Modo Padrão: A cada 24 horas um novo bairro sera escolhido </h2>
+            <h2>
+              Modo Padrão: A cada 24 horas, um novo bairro será escolhido.
+            </h2>
             <p>
               1 - Você tem infinitas tentativas para acertar o bairro do dia.
             </p>
             <p>
-              2 - Todos os bairros possuiem um total de 5 imagens de diferentes
-              locais do bairro.
+              2 - Todos os bairros possuem um total de 5 imagens de diferentes
+              locais.
             </p>
             <p>
-              3 - A cada erro a imagem sera trocada para a proxima imagem do
+              3 - A cada erro, a imagem será trocada pela próxima imagem do
               mesmo bairro.
             </p>
           </li>
           <li className="comojogar-livre">
             <h2>
-              Modo Livre: No modo livre você basicamente joga um modo INFINITO
-              até você errar.
+              Modo Livre: No modo livre, você basicamente joga um modo infinito
+              até errar.
             </h2>
             <p>1 - Você tem até 5 tentativas por bairro.</p>
-            <p>2 - Caso você acerte suas tentavias serão zeradas.</p>
+            <p>2 - Caso você acerte, suas tentativas serão zeradas.</p>
             <p>
-              3 - A cada erro a imagem sera trocada para a proxima imagem do
+              3 - A cada erro, a imagem será trocada pela próxima imagem do
               mesmo bairro.
             </p>
-            <p>4 - Se você perder todas tentativas é GG.</p>
+            <p>4 - Se você perder todas as tentativas, é GG.</p>
           </li>
         </ul>
       </div>
